680d4f2fd4c8d725671e9f7b0882bdd657b89e85
Rudi Wijaya <rudi.mail90@gmail.com>
2019-01-29 09:52:53 +0700

    with error handler

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+import Modal from '../../components/UI/Modal/Modal';
+import Aux from '../Auxiliary/Auxiliary';
+
+const withErrorHandler = (WrappedComponent, axios) => {
+    return class extends Component {
+
+        state = {
+            error: null
+        }
+
+        componentDidMount() {
+            axios.interceptors.request.use(upReq => {
+                this.setState({error: null});
+
+                return upReq;
+            })
+            axios.interceptors.response.use(upResp => upResp, upError => {
+                console.log(upError);
+                this.setState({error: upError})
+            });
+        }
+
+        dismissError = () => {
+            this.setState({error: null});
+        }
+
+        render() {
+            return (
+                <Aux>
+                    <Modal
+                        show={this.state.error}
+                        dismissModal={this.dismissError} >
+                        {this.state.error ? this.state.error.message : null}
+                    </Modal>
+                    <WrappedComponent {...this.props} />
+                </Aux>
+            );
+        }
+    }
+}
+
+export default withErrorHandler;
